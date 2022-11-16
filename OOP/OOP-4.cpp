4f8dd7269fb0bedbc3892f0e0a8dfca5c545a975
sedtia-prakoso-budi-tirto-arto <99130485+sedtia-prakoso-budi-tirto-arto@users.noreply.github.com>
2022-11-16 09:12:22 +0700

    Add files via upload

---
+++ OOP/OOP-4.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Vertex
+{
+protected:
+    list<int> adj;
+
+public:
+    void addEdge(int u, int v)
+    {
+        if (adj.empty())
+            adj.push_back(u);
+        adj.push_back(v);
+    }
+
+    void showList()
+    {
+        list<int>::iterator it;
+        for (it = adj.begin(); it != adj.end(); ++it)
+        {
+            cout << *it << " ";
+        }
+        cout << "\n";
+    }
+};
+
+class Graph
+{
+protected:
+    int length;
+    Vertex vertex[100];
+
+public:
+    Graph(int _length)
+    {
+        length = _length;
+    }
+
+    AddEdgetoVertex(int u, int v)
+    {
+        vertex[u].addEdge(u, v);
+        vertex[v].addEdge(v, u);
+    }
+
+    printVertex()
+    {
+        for (int i = 0; i < length; i++)
+        {
+            vertex[i].showList();
+        }
+    }
+};
+
+int main()
+{
+    Graph graph1(5);
+
+    graph1.AddEdgetoVertex(0, 1);
+    graph1.AddEdgetoVertex(0, 4);
+    graph1.AddEdgetoVertex(1, 2);
+    graph1.AddEdgetoVertex(1, 3);
+    graph1.AddEdgetoVertex(1, 4);
+    graph1.AddEdgetoVertex(2, 3);
+    graph1.AddEdgetoVertex(3, 4);
+
+    graph1.printVertex();
+}
